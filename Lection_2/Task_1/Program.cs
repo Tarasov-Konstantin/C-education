238f321f25b74c472f93d73f8cfca29cf53eb71f
Konstantin Tarasov <grch@yandex.ru>
2022-09-19 19:59:46 +0300

    pre practice 2

---
+++ Lection_2/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿int Max(int arg1, int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+int a1 = 10;
+int a2 = 21;
+int a3 = 53;
+int a4 = 88;
+int a5 = 439;
+int a6 = 99;
+int a7 = 18;
+int a8 = 23;
+int a9 = 69;
+
+int max = a1;
+if(a2>max) max = a2;
+if(a3>max) max = a3;
+if(a4>max) max = a4;
+if(a5>max) max = a5;
+if(a6>max) max = a6;
+
+//int max1 = Max(a1, a2, a3);
+//int max2 = Max(a4, a5, a6);
+//int max3 = Max(a7, a8, a9);
+//int maxmax = Max(max1, max2, max3);
+
+int maxmax = Max(
+    Max(a1, a2, a3), 
+    Max(a4, a5, a6), 
+    Max(a7, a8, a9));
+
+Console.WriteLine(max);
+Console.WriteLine(maxmax);
---
+++ Lection_2/Task_2 array/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int arg1, int arg2,int arg3)
+{
+    int result = arg1;
+    if(arg2>result) result = arg2;
+    if(arg3>result) result = arg3;
+    return result;
+}
+
+int[] array = {14,20,23,14,75,86,27,68,49};
+//array[0] = 12;
+//Console.WriteLine(array[0]);
+
+//int max1 = Max(a1, a2, a3);
+//int max2 = Max(a4, a5, a6);
+//int max3 = Max(a7, a8, a9);
+//int maxmax = Max(max1, max2, max3);
+
+int maxmax = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8]));
+
+
+Console.WriteLine(maxmax);
